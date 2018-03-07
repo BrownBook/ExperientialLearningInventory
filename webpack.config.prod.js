d41beceb841b1a5e72ef6b0da1f42d4172aaea42
Jeremy Booker <jeremy.booker@gmail.com>
2018-03-06 22:57:08 -0500

    Fix webpack config merge conflict

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,29 +12,7 @@ module.exports = {
     // Don't attempt to continue if there are any errors
     bail: true,
     devtool: 'source-map',
-<<<<<<< ae6c8f2440d8e4d0a5688e4c9911a646bd2b7749
     entry: entryPointList.entryPoints,
-=======
-    entry: {
-        createInterface: JS_DIR + '/createInterface/CreateInternshipInterface.jsx',
-        searchInterface: JS_DIR + '/searchInterface/SearchInterface.jsx',
-        editAdmin: JS_DIR + '/editAdmin/editAdmin.jsx',
-        editDepartment: JS_DIR + '/editDepartment/deptEditor.jsx',
-        stateList: JS_DIR + '/stateList/StateList.jsx',
-        emergencyContact: JS_DIR + '/emergencyContact/EmgContactList.jsx',
-        facultyEdit: JS_DIR + '/facultyEdit/FacultyEdit.jsx',
-        editMajor: JS_DIR + '/editMajor/editMajor.jsx',
-        editGrad: JS_DIR + '/editGrad/editGrad.jsx',
-        affiliationDepartments: JS_DIR + '/affiliationAgreement/AffiliationDepartments.jsx',
-        affiliateList: JS_DIR + '/affiliationAgreement/AffiliateList.jsx',
-        affiliationLocation: JS_DIR + '/affiliationAgreement/AffiliationLocation.jsx',
-        affiliationTerminate: JS_DIR + '/affiliationAgreement/AffiliationTerminate.jsx',
-        editExpectedCourses: JS_DIR + '/editCourses/courseEditor.jsx',
-        majorSelector: JS_DIR + '/majorSelector/MajorSelector.jsx',
-        adminSettings: JS_DIR + '/settings/settings.jsx',
-        vendor: ['jquery', 'react', 'react-dom', 'react-bootstrap']
-    },
->>>>>>> Render for settings.jsx
     output: {
         path: path.join(JS_DIR, "dist"),
         filename: "[name].[chunkhash:8].min.js",
