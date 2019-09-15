e535c52211c534bf7025606193c5989f5b17b0b4
Jeremy Booker <jeremy.booker@gmail.com>
2019-09-15 11:02:31 -0400

    Re-order create table statements in install.sql so that intern_import exists before being referenced by intern_internship.import_id

--- boost/install.sql
+++ boost/install.sql
@@ -564,6 +564,40 @@ create table intern_student_autocomplete (
     PRIMARY KEY(banner_id)
 );
 
+
+CREATE TABLE intern_import (
+    id INT NOT NULL,
+    name character varying NOT NULL,
+    uploaded_timestamp INT NOT NULL,
+    uploaded_by character varying NOT NULL,
+    imported_timestamp INT,
+    validated_on INT,
+    PRIMARY KEY(id)
+);
+
+CREATE SEQUENCE intern_import_seq;
+
+CREATE TABLE intern_import_activity (
+    id          INT NOT NULL,
+    import_id   INT NOT NULL REFERENCES intern_import(id),
+    row_num     INT NOT NULL,
+    student_id   character varying,
+    first_name  character varying,
+    last_name   character varying,
+    term        character varying,
+    level       character varying,
+    experience_type character varying,
+    host_name       character varying,
+    host_state      character varying,
+    department_name character varying,
+    validation_errors character varying,
+    validated_on INT,
+    PRIMARY KEY(id)
+);
+
+CREATE SEQUENCE intern_import_activity_seq;
+
+
 CREATE TABLE intern_internship (
        id INT NOT NULL,
        term character varying NOT NULL REFERENCES intern_term(term),
@@ -866,37 +900,5 @@ CREATE TABLE intern_local_student_data (
 
     primary key(student_id)
 );
-
-CREATE TABLE intern_import (
-    id INT NOT NULL,
-    name character varying NOT NULL,
-    uploaded_timestamp INT NOT NULL,
-    uploaded_by character varying NOT NULL,
-    imported_timestamp INT,
-    validated_on INT,
-    PRIMARY KEY(id)
-);
-
-CREATE SEQUENCE intern_import_seq;
-
-CREATE TABLE intern_import_activity (
-    id          INT NOT NULL,
-    import_id   INT NOT NULL REFERENCES intern_import(id),
-    row_num     INT NOT NULL,
-    student_id   character varying,
-    first_name  character varying,
-    last_name   character varying,
-    term        character varying,
-    level       character varying,
-    experience_type character varying,
-    host_name       character varying,
-    host_state      character varying,
-    department_name character varying,
-    validation_errors character varying,
-    validated_on INT,
-    PRIMARY KEY(id)
-);
-
-CREATE SEQUENCE intern_import_activity_seq;
 
 COMMIT;
