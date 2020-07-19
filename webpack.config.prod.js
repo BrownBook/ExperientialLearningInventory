06771f108ac6b296b554315547a71d7be0c8cbf2
Jeremy Booker <jeremy.booker@gmail.com>
2020-07-19 09:36:49 -0400

    Webpack v4 works.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,6 +9,7 @@ var entryPointList = require(__dirname + '/entryPoints.js');
 var JS_DIR = path.resolve(__dirname, 'javascript');
 
 module.exports = {
+    mode: 'production',
     // Don't attempt to continue if there are any errors
     bail: true,
     devtool: 'source-map',
@@ -40,7 +41,7 @@ module.exports = {
             use: [
               {
                 loader: 'babel-loader',
-                query: {presets: ['es2015', 'react']}
+                query: {presets: ['env', 'react']}
               }
             ]
 
@@ -61,28 +62,11 @@ module.exports = {
         }]
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
         //new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.[chunkhash:8].bundle.js"),
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.[chunkhash:8].bundle.js' }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                screw_ie8: true, // React doesn't support IE8 anyway
-                warnings: false
-            },
-            mangle: {
-                screw_ie8: true
-            },
-            output: {
-                comments: false,
-                screw_ie8: true
-            }
-        }),
+        //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.[chunkhash:8].bundle.js' }),
         new AssetsPlugin({
             filename: 'assets.json',
             prettyPrint: true
-        })    ]
+        })
+    ]
 }
