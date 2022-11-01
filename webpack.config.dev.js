7acdbf398b2ed142e1eb573e0854bcea3506f787
Jeremy Booker <jeremy.booker@gmail.com>
2022-11-01 17:21:43 -0400

    Update to Webpack 5

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,70 +1,55 @@
-var webpack = require('webpack');
-var path = require('path');
-var Promise = require('es6-promise').polyfill();
-var AssetsPlugin = require('assets-webpack-plugin');
-var entryPointList = require(__dirname + '/entryPoints.js');
+var webpack = require("webpack");
+var path = require("path");
+// var Promise = require("es6-promise").polyfill();
+var AssetsPlugin = require("assets-webpack-plugin");
+var entryPointList = require(__dirname + "/entryPoints.js");
 
-//var APP_DIR = path.resolve(__dirname, '');
-var JS_DIR = path.resolve(__dirname, 'javascript');
+// TODO
+// const ESLintPlugin = require("eslint-webpack-plugin");
 
-
+var JS_DIR = path.resolve(__dirname, "javascript");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval',
-    entry: entryPointList.entryPoints,
-    output: {
-        path: path.join(JS_DIR, "dist"),
-        filename: "[name].dev.js",
-        publicPath: "javascript/dist/"
-    },
-    externals: {
-        "jquery": "$"
-    },
-    watchOptions: {
-        ignored: ['**/node_modules/**', '**/vendor/**']
-    },
-    module: {
-        rules: [
-        {
-            enforce: "pre",
-            test: /\.(js|jsx)$/,
-            use: [
-              {
-                loader: "eslint-loader",
-                options: {configFile: path.join(__dirname, '.eslintrc.js'),
-                         useEslintrc: false}
-              }
-            ],
-            include: JS_DIR
-        }, {
-            test: /\.(js|jsx)$/,
-            include: JS_DIR,
-            use: [
-              {
-                loader: 'babel-loader',
-                query: {presets: ['env', 'react']}
-              }
-            ]
-        }, {
-            test: /\.css$/,
-            use: [
-              {
-                loader: "style-loader"
-              },
-              {
-                loader: "css-loader"
-              }
-            ]
-        }]
-    },
-    plugins: [
-        new AssetsPlugin({
-            filename: 'assets.json',
-            prettyPrint: true
-        })
-        //new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
-        //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
-
-    ]
-}
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: entryPointList.entryPoints,
+  output: {
+    path: path.join(JS_DIR, "dist"),
+    filename: "[name].dev.js",
+    publicPath: "javascript/dist/",
+  },
+  externals: {
+    jquery: "$",
+  },
+  watchOptions: {
+    ignored: ["**/node_modules/**", "**/vendor/**"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: JS_DIR,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new AssetsPlugin({
+      filename: "assets.json",
+      prettyPrint: true,
+    }),
+    //new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
+    //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+  ],
+};
