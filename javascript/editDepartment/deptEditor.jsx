07b8b134b901371f1a899fba1198d96ed4dedc04
Jeremy Booker <jeremy.booker@gmail.com>
2022-12-17 15:38:58 -0500

    JS cleanup in admin, course, level, major, and grad program editors.

--- javascript/editDepartment/deptEditor.jsx
+++ javascript/editDepartment/deptEditor.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import Manager from '../manager/Manager.jsx';
 
 ReactDOM.render(
-	<Manager ajaxURL="deptRest"
-            title="Departments"
-            panelTitle="Add A Department "
-            buttonTitle="Add Department"  />,
-	document.getElementById('content')
+  <Manager ajaxURL="deptRest" title="Departments" panelTitle="Add A Department " buttonTitle="Add Department" />,
+  document.getElementById('content')
 );
