7429f007edd4c464e86d6cea6add8db7fc368fb9
Jeremy Booker <jeremy.booker@gmail.com>
2022-12-17 17:13:54 -0500

    JS Cleanup in abstract Manager component.

--- javascript/manager/Manager.jsx
+++ javascript/manager/Manager.jsx
@@ -3,305 +3,330 @@ import $ from 'jquery';
 import PropTypes from 'prop-types';
 
 class AddData extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
-        this.addDataRef = React.createRef();
-    }
-	handleClick() {
-		var textName = this.addDataRef.current.value.trim();
-		this.props.onCreate(textName);
-	}
-	render() {
-		return (
-			<div className="col-md-5 col-md-offset-1">
-				<br /><br /><br />
-				<div className="panel panel-default">
-					<div className="panel-body">
-						<div className="row">
-							<div className="col-md-10">
-								<label>{this.props.panelTitle}</label>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-8">
-                                <div className="form-group">
-                                <input type="text" className="form-control" ref={this.addDataRef} />
-                                </div>
-							</div>
+    this.addDataRef = React.createRef();
+  }
 
-							<div className="col-md-4">
-                                <div className="form-group">
-                                <button className="btn btn-default btn-md" onClick={this.handleClick}>{this.props.buttonTitle}</button>
-                                </div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
+  handleClick() {
+    this.props.onCreate(this.addDataRef.current.value.trim());
+  }
 
-	}
+  render() {
+    return (
+      <div className="col-md-5 col-md-offset-1">
+        <br />
+        <br />
+        <br />
+        <div className="panel panel-default">
+          <div className="panel-body">
+            <div className="row">
+              <div className="col-md-10">
+                <label>{this.props.panelTitle}</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-8">
+                <div className="form-group">
+                  <input type="text" className="form-control" ref={this.addDataRef} />
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className="form-group">
+                  <button className="btn btn-default btn-md" onClick={this.handleClick}>
+                    {this.props.buttonTitle}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 AddData.propTypes = {
-    panelTitle: PropTypes.string,
-    buttonTitle: PropTypes.string,
-    onCreate: PropTypes.func
-}
-
+  panelTitle: PropTypes.string,
+  buttonTitle: PropTypes.string,
+  onCreate: PropTypes.func
+};
 
 class DisplayData extends React.Component {
-	constructor(props) {
-        super(props);
-		this.state = {
-			editMode: false
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: false
+    };
 
-        this.handleEdit = this.handleEdit.bind(this);
-        this.handleHide = this.handleHide.bind(this);
-        this.handleSave = this.handleSave.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+    this.handleSave = this.handleSave.bind(this);
 
-        this.savedDataRef = React.createRef();
-	}
-	handleEdit() {
-		this.setState({editMode: true});
+    this.savedDataRef = React.createRef();
+  }
 
-	}
-	handleHide() {
-		this.props.onHidden(this.props.hidden, this.props.id);
-	}
-	handleSave() {
-		this.setState({editMode: false});
+  handleEdit() {
+    this.setState({ editMode: true });
+  }
 
-		// Grabs the value in the textbox
-		var newName = this.savedDataRef.current.value.trim();
+  handleHide() {
+    this.props.onHidden(this.props.hidden, this.props.id);
+  }
 
-		if (newName === '')
-		{
-			newName = this.props.name;
-		}
+  handleSave() {
+    this.setState({ editMode: false });
 
-		var originalName = this.props.name;
+    // Grabs the value in the textbox
+    let newName = this.savedDataRef.current.value.trim();
 
-		this.props.onSave(originalName, newName, this.props.id);
-	}
-	render() {
-        var name = null;
-        var hButton = null;
-		// Determines which element to show on the page (hide/show and Save/Edit)
-		if (this.props.hidden === 0) {
-			name = this.props.name;
-			hButton = <button className="btn btn-default btn-xs" type="submit" onClick={this.handleHide}> Hide </button>
-		} else {
-			name = <span className="text-muted"><em> {this.props.name} </em></span>;
-			hButton = <button className="btn btn-default btn-xs" type="submit" onClick={this.handleHide}> Show </button>
-		}
+    if (newName === '') {
+      newName = this.props.name;
+    }
 
-        var text = null;
-        var eButton = null;
-		if (this.state.editMode) {
-			//var eName = 'Save';
-			text = <div id={this.props.id} >
-                        <input type="text" className="form-control" defaultValue={this.props.name} ref={this.savedDataRef} />
-					</div>
+    this.props.onSave(this.props.name, newName, this.props.id);
+  }
 
-			eButton = <button className="btn btn-default btn-xs" type="submit" onClick={this.handleSave}> Save </button>
-		} else {
-			//var eName = 'Edit';
-			text = name;
+  render() {
+    let name = null;
+    let hButton = null;
+    // Determines which element to show on the page (hide/show and Save/Edit)
+    if (this.props.hidden === 0) {
+      name = this.props.name;
+      hButton = (
+        <button className="btn btn-default btn-xs" type="submit" onClick={this.handleHide}>
+          {' '}
+          Hide{' '}
+        </button>
+      );
+    } else {
+      name = (
+        <span className="text-muted">
+          <em> {this.props.name} </em>
+        </span>
+      );
+      hButton = (
+        <button className="btn btn-default btn-xs" type="submit" onClick={this.handleHide}>
+          {' '}
+          Show{' '}
+        </button>
+      );
+    }
 
-			eButton = <button className="btn btn-default btn-xs" type="submit" onClick={this.handleEdit}> Edit </button>
-		}
+    let text = null;
+    let eButton = null;
+    if (this.state.editMode) {
+      //var eName = 'Save';
+      text = (
+        <div id={this.props.id}>
+          <input type="text" className="form-control" defaultValue={this.props.name} ref={this.savedDataRef} />
+        </div>
+      );
 
-		return (
-			<tr>
-				<td>{text}</td>
-				<td>{eButton}</td>
-				<td>{hButton}</td>
-			</tr>
+      eButton = (
+        <button className="btn btn-default btn-xs" type="submit" onClick={this.handleSave}>
+          {' '}
+          Save{' '}
+        </button>
+      );
+    } else {
+      //var eName = 'Edit';
+      text = name;
 
-		);
+      eButton = (
+        <button className="btn btn-default btn-xs" type="submit" onClick={this.handleEdit}>
+          {' '}
+          Edit{' '}
+        </button>
+      );
+    }
 
-	}
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{eButton}</td>
+        <td>{hButton}</td>
+      </tr>
+    );
+  }
 }
 
 DisplayData.propTypes = {
-    id: PropTypes.any,
-    name: PropTypes.string,
-    hidden: PropTypes.any,
-    onSave: PropTypes.any,
-    onHidden: PropTypes.any
-}
-
+  id: PropTypes.any,
+  name: PropTypes.string,
+  hidden: PropTypes.any,
+  onSave: PropTypes.any,
+  onHidden: PropTypes.any
+};
 
 class Manager extends React.Component {
-	constructor(props) {
-        super(props);
-		this.state = {
-			mainData: null,
-			errorWarning: '',
-			success: ''
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainData: null,
+      errorWarning: '',
+      success: ''
+    };
 
-        this.getData = this.getData.bind(this);
-        this.onHidden = this.onHidden.bind(this);
-        this.onSave = this.onSave.bind(this);
-        this.onCreate = this.onCreate.bind(this);
-	}
-	componentDidMount(){
-		this.getData();
-	}
-	getData(){
-		$.ajax({
-			url: 'index.php?module=intern&action='+this.props.ajaxURL,
-			type: 'GET',
-			dataType: 'json',
-			success: function(data) {
-				this.setState({mainData: data});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				alert("Sorry, there was a problem fetching data from the server.");
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	}
-	onHidden(val, id){
-		// Hides the selected value
-		if (val === 0) {
-			val += 1;
-		} else {
-			val += -1;
-		}
+    this.getData = this.getData.bind(this);
+    this.onHidden = this.onHidden.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.onCreate = this.onCreate.bind(this);
+  }
 
-		$.ajax({
-			url: 'index.php?module=intern&action='+this.props.ajaxURL+'&val='+val+'&id='+id,
-			type: 'PUT',
-			success: function() {
-				this.getData();
-			}.bind(this),
-			error: function(xhr, status, err) {
-				alert("Sorry, there was a problem fetching data from the server.");
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	}
-	onSave(orgName, newName, id){
-		var cleanName = encodeURIComponent(newName)
+  componentDidMount() {
+    this.getData();
+  }
 
-		// Saves the value into the database
-		$.ajax({
-			url: 'index.php?module=intern&action='+this.props.ajaxURL+'&name='+cleanName+'&id='+id,
-			type: 'PUT',
-			success: function(data) {
-				// Determines if the values have changed and if so, continues
-				// with the changes.
-				if (orgName !== newName)
-				{
-					$("#success").show();
-					var added = 'Updated '+orgName+ " to " +newName+'.';
-					this.setState({success: added});
-				}
-				this.getData();
-			}.bind(this),
-			error: function(xhr, status, err) {
-				alert("Sorry, there was a problem fetching data from the server.");
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	}
-	onCreate(name) {
-		// Creates a new value
-		$.ajax({
-			url: 'index.php?module=intern&action='+this.props.ajaxURL+'&create='+name,
-			type: 'POST',
-			success: function(data) {
-				// Shows a success message for the new value being added.
-				$("#success").show();
-				var added = 'Added '+name+'.';
-				this.setState({success: added});
-				this.getData();
-			}.bind(this),
-			error: function(http) {
-				var errorMessage = http.responseText;
-				this.setState({errorWarning: errorMessage});
-				$("#warningError").show();
-			}.bind(this)
-		});
-	}
-	render() {
-        var data = null
-        var description = null
-		if (this.state.mainData != null) {
-			var onHidden = this.onHidden;
-			var onSave = this.onSave;
-			data = this.state.mainData.map(function (data) {
-                if(data.name == null){
-                    description = data.description
-                } else{
-                    description = data.name
-                }
-                return (
-                    <DisplayData key={data.id}
-                        id={data.id}
-						name={description}
-						hidden={data.hidden}
-						onHidden={onHidden}
-						onSave={onSave} />
-				);
-			});
+  getData() {
+    $.ajax({
+      url: 'index.php?module=intern&action=' + this.props.ajaxURL,
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        this.setState({ mainData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert('Sorry, there was a problem fetching data from the server.');
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
 
-		} else {
-			data = <tr><td></td></tr>;
-		}
+  onHidden(val, id) {
+    // Hides the selected value
+    if (val === 0) {
+      val += 1;
+    } else {
+      val += -1;
+    }
 
-		return (
-			<div className="data">
+    $.ajax({
+      url: 'index.php?module=intern&action=' + this.props.ajaxURL + '&val=' + val + '&id=' + id,
+      type: 'PUT',
+      success: function () {
+        this.getData();
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert('Sorry, there was a problem fetching data from the server.');
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
 
-			<div id="success" className="alert alert-success alert-dismissible" role="alert" hidden>
-				<button type="button"  className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<strong>Success!</strong> {this.state.success}
-			</div>
+  onSave(orgName, newName, id) {
+    const cleanName = encodeURIComponent(newName);
 
-			<div id="warningError" className="alert alert-warning alert-dismissible" role="alert" hidden>
-				<button type="button"  className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<strong>Warning!</strong> {this.state.errorWarning}
-			</div>
+    // Saves the value into the database
+    $.ajax({
+      url: 'index.php?module=intern&action=' + this.props.ajaxURL + '&name=' + cleanName + '&id=' + id,
+      type: 'PUT',
+      success: function (data) {
+        // Determines if the values have changed and if so, continues
+        // with the changes.
+        if (orgName !== newName) {
+          $('#success').show();
+          const added = 'Updated ' + orgName + ' to ' + newName + '.';
+          this.setState({ success: added });
+        }
+        this.getData();
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert('Sorry, there was a problem fetching data from the server.');
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
 
-				<div className="row">
-					<div className="col-md-5">
-						<h1> {this.props.title} </h1>
-							<table className="table table-condensed table-striped">
-								<thead>
-									<tr>
-										<th>Name</th>
-										<th>Options</th>
-										<th></th>
-									</tr>
-								</thead>
-								<tbody>
-									{data}
-								</tbody>
-							</table>
-					</div>
-					<AddData onCreate={this.onCreate}
-                                buttonTitle={this.props.buttonTitle}
-                                panelTitle={this.props.panelTitle}  />
+  onCreate(name) {
+    // Creates a new value
+    $.ajax({
+      url: 'index.php?module=intern&action=' + this.props.ajaxURL + '&create=' + name,
+      type: 'POST',
+      success: function (data) {
+        // Shows a success message for the new value being added.
+        $('#success').show();
+        const added = 'Added ' + name + '.';
+        this.setState({ success: added });
+        this.getData();
+      }.bind(this),
+      error: function (http) {
+        const errorMessage = http.responseText;
+        this.setState({ errorWarning: errorMessage });
+        $('#warningError').show();
+      }.bind(this)
+    });
+  }
 
-				</div>
-			</div>
-		);
-	}
+  render() {
+    let data = null;
+    let description = null;
+    if (this.state.mainData != null) {
+      const onHidden = this.onHidden;
+      const onSave = this.onSave;
+      data = this.state.mainData.map(function (data) {
+        if (data.name == null) {
+          description = data.description;
+        } else {
+          description = data.name;
+        }
+        return <DisplayData key={data.id} id={data.id} name={description} hidden={data.hidden} onHidden={onHidden} onSave={onSave} />;
+      });
+    } else {
+      data = (
+        <tr>
+          <td></td>
+        </tr>
+      );
+    }
+
+    return (
+      <div className="data">
+        <div id="success" className="alert alert-success alert-dismissible" role="alert" hidden>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <strong>Success!</strong> {this.state.success}
+        </div>
+
+        <div id="warningError" className="alert alert-warning alert-dismissible" role="alert" hidden>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <strong>Warning!</strong> {this.state.errorWarning}
+        </div>
+
+        <div className="row">
+          <div className="col-md-5">
+            <h1> {this.props.title} </h1>
+            <table className="table table-condensed table-striped">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Options</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>{data}</tbody>
+            </table>
+          </div>
+          <AddData onCreate={this.onCreate} buttonTitle={this.props.buttonTitle} panelTitle={this.props.panelTitle} />
+        </div>
+      </div>
+    );
+  }
 }
 
 Manager.propTypes = {
-    url: PropTypes.string,
-    buttonTitle: PropTypes.string,
-    panelTitle: PropTypes.string,
-    title: PropTypes.string,
-    ajaxURL: PropTypes.string,
-}
+  url: PropTypes.string,
+  buttonTitle: PropTypes.string,
+  panelTitle: PropTypes.string,
+  title: PropTypes.string,
+  ajaxURL: PropTypes.string
+};
 
 export default Manager;
