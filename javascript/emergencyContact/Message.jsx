ae0143952eb8129dd85e3812155b6ed6a37f01ee
Jeremy Booker <jeremy.booker@gmail.com>
2022-12-17 17:06:46 -0500

    JS cleanup in emergency contact components

--- javascript/emergencyContact/Message.jsx
+++ javascript/emergencyContact/Message.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * This file is part of Internship Inventory.
@@ -21,56 +21,51 @@ import PropTypes from 'prop-types'
  */
 
 class Message extends Component {
-
   render() {
-    let icon = ''
+    let icon = '';
     switch (this.props.type) {
       case 'danger':
-        icon = 'fa fa-exclamation-triangle'
-        break
+        icon = 'fa fa-exclamation-triangle';
+        break;
 
       case 'success':
-        icon = 'fa fa-thumbs-o-up'
-        break
+        icon = 'fa fa-thumbs-o-up';
+        break;
 
       case 'info':
-        icon = 'fa fa-info-circle'
-        break
+        icon = 'fa fa-info-circle';
+        break;
 
       case 'warning':
-        icon = 'fa fa-hand-paper-o'
-        break
-        
+        icon = 'fa fa-hand-paper-o';
+        break;
+
       default:
-        icon = 'fa fa-info-circle'
+        icon = 'fa fa-info-circle';
     }
 
-    let messageType = 'alert alert-dismissible alert-' + this.props.type
+    const messageType = 'alert alert-dismissible alert-' + this.props.type;
+
     return (
       <div className={messageType} role="alert">
-        <button
-          type="button"
-          onClick={this.props.onClose}
-          className="close"
-          data-dismiss="alert"
-          aria-label="Close">
+        <button type="button" onClick={this.props.onClose} className="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
         <i className={icon}></i>&nbsp;
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
 Message.propTypes = {
   type: PropTypes.string,
-  children: PropTypes.oneOfType([PropTypes.string,PropTypes.element]),
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   onClose: PropTypes.func
-}
+};
 
 Message.defaultProps = {
   type: 'info'
-}
+};
 
-export default Message
+export default Message;
