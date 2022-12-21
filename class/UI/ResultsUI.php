5e9207ab77246393d5b8385e15286f2a13d8f2f8
Jeremy Booker <jeremy.booker@gmail.com>
2022-12-20 20:42:33 -0500

    Fix use of count() in search results UI for PHP 7.2

--- class/UI/ResultsUI.php
+++ class/UI/ResultsUI.php
@@ -119,7 +119,7 @@ class ResultsUI implements UI
 
 
         // If there were no results, send the user back to the search interface
-        if (sizeof($pager->display_rows) == 0) {
+        if ($pager->display_rows === null) {
             \NQ::simple('intern', NotifyUI::WARNING, "There were no internships that matched your search criteria. If you're looking for a specific student double check the student's name, id number, or email address. Otherwise, try selecting fewer search criteria and then search again.");
             \NQ::close();
 
